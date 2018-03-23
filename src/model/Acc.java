7c444b6b2163e432141316c4d0af6a36e506f522
angelinovic18 <marcogila94@gmail.com>
2018-03-23 16:24:55 +0100

    model

---
+++ src/model/Acc.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Acc {
+
+	private int id;
+	private int data_inizio;
+	private int id_corsista;
+	private int id_azienda_corso;
+	
+	public Acc(int id, int data_inizio, int id_corsista, int id_azienda_corso){
+		this.id = id;
+		this.data_inizio = data_inizio;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public int getData_inizio(){
+		return data_inizio;
+	}
+	
+	public void setData_inizio(int data_inizio){
+		this.data_inizio = data_inizio;
+	}
+	
+	public int getId_corsista(){
+		return id_corsista;
+	}
+	
+	public void setId_corsista(int id_corsista){
+		this.id_corsista = id_corsista;
+	}
+	
+	public int getId_azienda_corso(){
+		return id_azienda_corso;
+	}
+	
+	public void setId_azienda_corso(int id_azienda_corso){
+		this.id_azienda_corso = id_azienda_corso;
+	}
+}
---
+++ src/model/Azienda.java
@@ -0,0 +1,154 @@
+package model;
+
+
+public class Azienda {
+
+	private int id;
+	private String nome;
+	private String comune;
+	private String rappresentante;
+	private String sede_legale;
+	private String sede_operativa;
+	private String codice_fiscale;
+	private String iva;
+	private String email;
+	private String pec;
+	private String cellulare;
+	private String telefono;
+	private String ateco;
+	private String note;
+	
+	public Azienda(int id, String nome, String comune, String rappresentante, String sede_legale, String sede_operativa, String codice_fiscale, String iva, String email, String pec, String cellulare, String telefono, String ateco, String note){
+		this.id = id;
+		this.nome = nome;
+		this.comune = comune;
+		this.rappresentante = rappresentante;
+		this.sede_legale = sede_legale;
+		this.sede_operativa = sede_operativa;
+		this.codice_fiscale = codice_fiscale;
+		this.iva = iva;
+		this.email = email;
+		this.pec = pec;
+		this.cellulare = cellulare;
+		this.telefono = telefono;
+		this.ateco = ateco;
+		this.note = note;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getComune(){
+		return comune;
+	}
+	
+	public void setComune(String comune){
+		this.comune = comune;
+	}
+	
+	public String getRappresentante(){
+		return rappresentante;
+	}
+	
+	public void setRappresentante(String rappresentante){
+		this.rappresentante = rappresentante;
+	}
+	
+	public String getSede_legale(){
+		return sede_legale;
+	}
+	
+	public void setSede_legale(String sede_legale){
+		this.sede_legale = sede_legale;
+	}
+	
+	public String getSede_operativa(){
+		return sede_operativa;
+	}
+	
+	public void setSede_operativa(String sede_operativa){
+		this.sede_operativa = sede_operativa;
+	}
+	
+	public String getCodice_fiscale(){
+		return codice_fiscale;
+	}
+	
+	public void setCodice_fiscale(String codice_fiscale){
+		this.codice_fiscale = codice_fiscale;
+	}
+	
+	public String getIva(){
+		return iva;
+	}
+	
+	public void setIva(String iva){
+		this.iva = iva;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getPec(){
+		return pec;
+	}
+	
+	public void setPec(String pec){
+		this.pec = pec;
+	}
+	
+	public String getCellulare(){
+		return cellulare;
+	}
+	
+	public void setCellulare(String cellulare){
+		this.cellulare = cellulare;
+	}
+	
+	public String getTelefono(){
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono){
+		this.telefono = telefono;
+	}
+	
+	public String getAteco(){
+		return ateco;
+	}
+	
+	public void setAteco(String ateco){
+		this.ateco = ateco;
+	}
+	
+	public String getNote(){
+		return note;
+	}
+	
+	public void setNote(String note){
+		this.note = note;
+	}
+	
+	
+	
+	
+	
+}
