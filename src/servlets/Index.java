1367797a694d3c7e98ab1e112af3cecff84ecbfe
moras18 <claudio.more94@gmail.com>
2018-03-23 15:50:37 +0100

    prova prima servlet
    prova index

---
+++ src/servlets/Index.java
@@ -0,0 +1,47 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.FreeMarker;
+
+/**
+ * Servlet implementation class Index
+ */
+@WebServlet("/Index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		  response.sendRedirect("Index");
+		
+    	
+   		 //FreeMarker.process("index.html", data, response, getServletContext());
+   		}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
