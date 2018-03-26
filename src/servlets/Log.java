03608bbcce9d51428c983b31597f45c064ce5825
moras18 <claudio.more94@gmail.com>
2018-03-26 16:55:08 +0200

    prova prime servlet
    servlet login e home

--- src/servlets/Log.java
+++ src/servlets/Log.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,28 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 import util.FreeMarker;
 
 /**
- * Servlet implementation class Index
+ * Servlet implementation class Log
  */
-@WebServlet("/Index")
-public class Index extends HttpServlet {
+@WebServlet("/Log")
+public class Log extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	Map data= new HashMap<String, Object>();  
     /**
      * @see HttpServlet#HttpServlet()
      */
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		  response.sendRedirect("Index");
-		
-    	
-   		 //FreeMarker.process("index.html", data, response, getServletContext());
-   		}
+    public Log() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		FreeMarker.process("login.html", data, response, getServletContext());
 	}
 
 	/**
@@ -41,7 +40,7 @@ public class Index extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		FreeMarker.process("home.html", data, response, getServletContext());
 	}
 
 }
