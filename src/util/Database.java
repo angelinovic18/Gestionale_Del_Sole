c3bed6d6b3a1b32d79ffea18e218aa471caaedf6
moras18 <claudio.more94@gmail.com>
2018-04-10 21:39:24 +0200

    finish finish finish

--- src/util/Database.java
+++ src/util/Database.java
@@ -16,7 +16,7 @@ public class Database {
 	protected static String DRIVER = "com.mysql.jdbc.Driver";
 	protected static String url = "jdbc:mysql://95.110.171.18/demo64";
 	protected static String user = "demo64";
-	protected static String psw = "demo8810";
+	protected static String psw = "demo5760";
 
 
 	private static Connection db;
